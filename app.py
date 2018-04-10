b9d1c3b5e1059f192316b9f7cae3284a6a60b7a3
gaogaotiantian <gaogaotiantian@hotmail.com>
2018-04-10 15:05:14 -0700

    remove useless import

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 # our packages
-from pyzbar import pyzbar as zbar
 from QRCodeReader import QRCodeReader
 
 if os.environ.get('DATABASE_URL') != None:
