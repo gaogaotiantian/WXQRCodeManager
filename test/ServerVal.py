e6ac1cbbe0f5b7fea6f92d6bdfb4819b53420ec5
shenmo3 <ahliu@umail.ucsb.edu>
2018-04-19 17:36:22 -0700

    add cml arg for separate test for different func. Delete err_msg assert update for group_get and group_post

--- test/ServerVal.py
+++ test/ServerVal.py
@@ -4,7 +4,6 @@ import argparse
 
 
 class qrcode_get(unittest.TestCase):
-
     def setUp(self):
         r = requests.get(url + '/')
         if r.status_code != 200:
@@ -17,45 +16,51 @@ class qrcode_get(unittest.TestCase):
 
     def test_get_id0(self):
         r = requests.get(url + '/api/v1/qrcode?id=0')
-        assert r.json()["err_msg"] == "Cannot find the id in database"
         assert r.status_code == 404
 
-    def test_get_id10000(self):
-        r = requests.get(url + '/api/v1/qrcode?id=10000')
-        assert r.json()["err_msg"] == "Cannot find the id in database"
+    def test_get_id_negative(self):
+        r = requests.get(url + '/api/v1/qrcode?id=-1')
         assert r.status_code == 404
 
     def test_get_invald_id(self):
         r = requests.get(url + '/api/v1/qrcode?id=stub')
-        assert r.json()["err_msg"] == "You need to input a valid id"
         assert r.status_code == 400
 
     def test_get_id_without_value(self):
         r = requests.get(url + '/api/v1/qrcode?id=')
-        assert r.json()["err_msg"] == "You need to input a valid id"
         assert r.status_code == 400
 
     def test_get_noId(self):
         r = requests.get(url + '/api/v1/qrcode')
-        assert r.json()["err_msg"] == "You need to input a valid id"
         assert r.status_code == 400
 
     def test_get_random_argument(self):
         r = requests.get(url + '/api/v1/qrcode?stub=stub')
-        assert r.json()["err_msg"] == "You need to input a valid id"
+        assert r.status_code == 400
+
+    def test_get_float(self):
+        assert requests.get(url + '/api/v1/qrcode?id=7.7').status_code
+
+    def test_get_id_space(self):
+        r = requests.get(url + '/api/v1/qrcode?id= ')
         assert r.status_code == 400
 
 
 class qrcode_post(unittest.TestCase):
-
     def setUp(self):
         r = requests.get(url + '/')
         if r.status_code != 200:
             self.skipTest("Server Error, Could not get root page")
 
 
-class groups(unittest.TestCase):
+class groups_get(unittest.TestCase):
+    def setUp(self):
+        r = requests.get(url + '/')
+        if r.status_code != 200:
+            self.skipTest("Server Error, Could not get root page")
 
+
+class groups_post(unittest.TestCase):
     def setUp(self):
         r = requests.get(url + '/')
         if r.status_code != 200:
@@ -63,14 +68,31 @@ class groups(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Enter Server URL')
-    parser.add_argument('input_url', type=str, nargs=1, help='Server URL to request')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_url', type=str, nargs=1, help="Server URL to request. Usage: python3 ServerVal.py "
+                                                             "http://127.0.0.1:5000")
+    parser.add_argument('test_code', nargs='?', default='5', help="1-5, default 5. 1 for qr_get, 2 for qr_post, 3 for "
+                                                                  "groups_get, 4 for groups_post, 5 for all test")
     args = parser.parse_args()
     url = args.input_url[0]
+    test_code = int(args.test_code)
     try:
         r = requests.get(url + '/')
     except:
         print("Connection Error, Please check if server is running and check url address")
     else:
+        if test_code < 1 or test_code > 5:
+            print("Please enter test code from 1-5, -h for help")
+            exit(0)
+        elif test_code == 1:
+            suite = unittest.TestLoader().loadTestsFromTestCase(qrcode_get)
+        elif test_code == 2:
+            suite = unittest.TestLoader().loadTestsFromTestCase(qrcode_post)
+        elif test_code == 3:
+            suite = unittest.TestLoader().loadTestsFromTestCase(groups_get)
+        elif test_code == 4:
+            suite = unittest.TestLoader().loadTestsFromTestCase(groups_post)
+        else:
+            unittest.main(verbosity=2, argv=['first-arg-is-ignored'], exit=False)
         # ignore the first arg because parser, verbosity set to 2 for more test info, change to 1 for less
-        unittest.main(verbosity=2,argv=['first-arg-is-ignored'], exit=False)
+        unittest.TextTestRunner(verbosity=2).run(suite)
