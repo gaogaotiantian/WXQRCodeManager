7f4b17b1f103e43f6d4931e45d6fff9dc5c53526
shenmo3 <ahliu@umail.ucsb.edu>
2018-05-03 17:43:53 -0700

    use unittest asserts, fixed bugs

--- test/ServerVal.py
+++ test/ServerVal.py
@@ -11,39 +11,39 @@ class qrcode_get(unittest.TestCase):
 
     def test_get_id1(self):
         r = requests.get(url + '/api/v1/qrcode?id=1')
-        assert r.status_code == 200
-        assert r.text
+        self.assertIsNotNone(r.status_code)
+        self.assertIsNotNone(r.text)
 
     def test_get_id0(self):
         r = requests.get(url + '/api/v1/qrcode?id=0')
-        assert r.status_code == 404
+        self.assertEqual(r.status_code, 404)
 
     def test_get_id_negative(self):
         r = requests.get(url + '/api/v1/qrcode?id=-1')
-        assert r.status_code == 404
+        self.assertEqual(r.status_code, 404)
 
     def test_get_invald_id(self):
         r = requests.get(url + '/api/v1/qrcode?id=stub')
-        assert r.status_code == 400
+        self.assertEqual(r.status_code, 400)
 
     def test_get_id_without_value(self):
         r = requests.get(url + '/api/v1/qrcode?id=')
-        assert r.status_code == 400
+        self.assertEqual(r.status_code, 400)
 
     def test_get_noId(self):
         r = requests.get(url + '/api/v1/qrcode')
-        assert r.status_code == 400
+        self.assertEqual(r.status_code, 400)
 
     def test_get_random_argument(self):
         r = requests.get(url + '/api/v1/qrcode?stub=stub')
-        assert r.status_code == 400
+        self.assertEqual(r.status_code, 400)
 
     def test_get_float(self):
-        assert requests.get(url + '/api/v1/qrcode?id=7.7').status_code
+        self.assertIsNotNone(requests.get(url + '/api/v1/qrcode?id=7.7').status_code)
 
     def test_get_id_space(self):
         r = requests.get(url + '/api/v1/qrcode?id= ')
-        assert r.status_code == 400
+        self.assertEqual(r.status_code, 400)
 
 
 class qrcode_post(unittest.TestCase):
@@ -55,12 +55,14 @@ class qrcode_post(unittest.TestCase):
     def test_post_random_qr(self):
         files = {'img': open('images/test_qrcode.png','rb')}
         r = requests.post(url + '/api/v1/qrcode', files=files)
-        assert r.status_code == 400
+        self.assertEqual(r.status_code, 400)
+        files['img'].close()
 
     def test_post_valid(self):
         files = {'img': open('images/myfile.jpg','rb')}
         r = requests.post(url + '/api/v1/qrcode', files=files)
-        assert r.status_code/100 == 2
+        self.assertEqual(int(r.status_code/100), 2)
+        files['img'].close()
 
 
 class groups_get(unittest.TestCase):
@@ -71,38 +73,39 @@ class groups_get(unittest.TestCase):
 
     def test_search_empty(self):
         r = requests.get(url + '/api/v1/groups?keywords= ')
+        self.assertEqual(r.status_code, 200)
 
     def test_search_no_arg(self):
         r = requests.get(url + '/api/v1/groups')
-        assert r.status_code == 200
+        self.assertEqual(r.status_code, 200)
 
     def test_search_invalid_arg(self):
         r = requests.get(url + '/api/v1/groups?keywords=')
-        assert r.status_code == 200
+        self.assertEqual(r.status_code, 200)
 
     def test_search_ucsb(self):
         r = requests.get(url + '/api/v1/groups?keywords=ucsb')
-        assert r.status_code == 200
+        self.assertEqual(r.status_code, 200)
         for i in r.json()['results']:
             tags = [x.lower() for x in i['tags']]
-            assert 'ucsb' in tags
+            self.assertIn('ucsb',tags)
         print()
         for i in r.json()['results']:
             self.qrget_search_result(str(i['id']))
 
     def test_search_cs(self):
         r = requests.get(url + '/api/v1/groups?keywords=cs')
-        assert r.status_code == 200
+        self.assertEqual(r.status_code, 200)
         for i in r.json()['results']:
             tags = [x.lower() for x in i['tags']]
-            assert 'cs' in tags
+            self.assertIn('cs', tags)
         print()
         for i in r.json()['results']:
             self.qrget_search_result(str(i['id']))
 
     def qrget_search_result(self,id):
         r = requests.get(url + '/api/v1/qrcode?id=' + id)
-        assert r.status_code == 200
+        self.assertEqual(r.status_code, 200)
         print("id" + id + " passed")
 
 
